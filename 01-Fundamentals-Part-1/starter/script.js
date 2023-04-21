511582c395ebff8655020e1326380aee28f52590
MounirDevWev <mounirabcire@gmail.com>
2023-04-21 21:24:41 +0100

    update

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,32 @@
+// 1- JS Linking File
+let js = 'amazing'
+//if (js === 'amazing') alert('JS Is Fun')
+console.log(10 - 10 + 15 + 100 - 10);
+
+// 2- Values And Variables
+let firstName = 'Mounir'
+let last_name = 'Abcire'
+let $function = 'student'
+let _new = 'new'
+
+console.log(firstName, last_name, $function);
+
+const PI = 3.14
+const HEIGHT = 20
+const WIDTH = 150
+let myFirstGame = 'valorant' // game1 = 'valorant'
+let mySecondGame = 'PES2023'// game2 = 'PES20023'
+
+// 3- Data Types
+let myFirstVar = 10
+let mySecondVar;
+
+// there are 7 primitive data types in JS
+console.log(typeof myFirstVar);
+console.log(typeof 'Mounir');
+myFirstVar = false
+console.log(typeof myFirstVar);
+console.log(typeof mySecondVar);
+console.log(typeof null); // this is a bug on JS, his type actuelly is null not an object
+//console.log(null instanceof null) ---> return an error
+// and 2 others which are: BigInt and Symbol
