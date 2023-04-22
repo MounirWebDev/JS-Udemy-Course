ee51f5101052d1fa01bdb654cd2b038c84510c89
MounirDevWev <mounirabcire@gmail.com>
2023-04-22 14:35:41 +0100

    update

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -30,3 +30,45 @@ console.log(typeof mySecondVar);
 console.log(typeof null); // this is a bug on JS, his type actuelly is null not an object
 //console.log(null instanceof null) ---> return an error
 // and 2 others which are: BigInt and Symbol
+
+// 4- Const, Let And Var
+let age = 18
+age = 19
+let empty
+
+const  YEAR = 2004
+
+job = 'studen'
+var job;
+job = 'front end developer'
+
+
+// 5- Basic Operators
+// math operators
+const CURRENTLY_YEAR = 2023
+let myAge = CURRENTLY_YEAR - 2004
+
+console.log(myAge, myAge + 15, myAge / 50, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 
+console.log('Abcire' + ' ' + 'Mounir')
+// assignement operators
+let x = 10 - 5;
+x++
+x--
+x+=5
+x-=5
+x*=5
+console.log(x);
+//comparison operators
+console.log(x > 5)// >, <, >=, <=
+
+// 6- Operator Precedence
+let a, b
+a = b = 10 - 5
+console.log((7 - 5) * 2);
+console.log(7 - 5 * 2);
+
+// **** CODING CHALLENGE #1 ****
+
+
+
