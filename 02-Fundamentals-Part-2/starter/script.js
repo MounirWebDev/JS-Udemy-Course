70c3254f50ab7401fcae59366f77abd05484ff74
MounirDevWev <mounirabcire@gmail.com>
2023-05-02 11:10:00 +0100

    finish the first challenge

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -37,3 +37,39 @@ const calcAge2 = (birthYear, currentYear) =>{
 
 console.log(calcAge(2004));
 console.log(calcAge2(2004, 2023));
+
+// 5-Functions Calling Other Functions
+
+function myFun(){
+    let a;
+    return  a = prompt("choose ur number: ");
+}
+
+function myFun2(result){
+    result = myFun() * myFun()
+    console.log(result)
+    return result
+}
+
+myFun2()
+
+// **** Coding Challenge #1 ****
+const calcAverage = (x, y, z) => (x + y + z) / 3
+const dolphlinsScore_data1 = calcAverage(44, 23, 71)
+const koalasScore_data1 = calcAverage(65, 54, 49)
+const dolphlinsScore_data2 = calcAverage(85, 54, 41)
+const koalasScore_data2 = calcAverage(23, 34, 27)
+console.log("DATA 1")
+checkWinner(dolphlinsScore_data1, koalasScore_data1)
+console.log("DATA 2")
+checkWinner(dolphlinsScore_data2, koalasScore_data2)
+
+function checkWinner(dolphlinsScore, koalasScore){
+    if(dolphlinsScore >= (koalasScore * 2)){
+        return console.log(`Dophin win (${dolphlinsScore} VS ${koalasScore})`)
+    }else if(koalasScore >= (dolphlinsScore * 2)){
+        return console.log(`Koala win (${koalasScore} VS ${dolphlinsScore})`)
+    }else{
+        return console.log("No team wins !")
+    }
+}
