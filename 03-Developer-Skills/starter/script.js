5e6726b2b4092e65d6bf71350b4df44957806d79
MounirDevWev <mounirabcire@gmail.com>
2023-05-10 16:26:05 +0100

    finish the challenge of the section

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,26 @@
 'use strict';
-let abc = 2;
 
+const x = () => {
+	const y = {
+		value: prompt('Degrees celsius'),
+	};
+	// using the debugger to find the error
+	let v = +y.value + 273;
+
+	return v;
+};
+console.log(x());
+
+// **** Coding Challenge #1 ****
+
+const printForecast = arr => {
+	let text = '...';
+
+	for (let i = 0; i < arr.length; i++) {
+		text += `${arr[i]}C in ${i + 1} days ...`;
+	}
+
+	console.log(text);
+};
+
+printForecast([12, 5, -5, 0, 4]);
